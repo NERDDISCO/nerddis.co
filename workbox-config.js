43a493c08d5f6562cd55665ba706245ce578c1ac
Tim Pietrusky <timpietrusky@gmail.com>
2020-04-23 10:56:36 +0200

    feat(pwa): Configuration for Workbox

---
+++ workbox-config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  "globDirectory": "public/",
+  "globPatterns": [
+    "**/*.{ico,svg,jpg,png,js,webp,webmanifest,html}"
+  ],
+  "globIgnores": [
+    "**/original/*",
+    "sw.js",
+    "sw.js.map",
+    "workbox-*",
+    "js/pwa-helper-components/test/*",
+    "js/pwa-helper-components/demo/**/*",
+    "js/pwa-helper-components/karma*",
+    "js/pwa-helper-components/pwa-dark-mode*",
+    "js/pwa-helper-components/pwa-dark-mode/*",
+    "js/pwa-helper-components/index.js",
+    "js/pwa-helper-components/pwa-install-button*",
+    "js/pwa-helper-components/pwa-install-button/*",
+    "js/pwa-helper-components/update-available-helper/update-available.js"
+  ],
+  "swDest": "public/sw.js"
+};
